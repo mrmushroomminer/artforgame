f29050a88fa61195d093044a951c658746d8dd2b
mrmushroomminer <81397821+mrmushroomminer@users.noreply.github.com>
2021-05-14 15:10:34 +1000

    trying to make jump animation - added l + r

--- main.ts
+++ main.ts
@@ -1,11 +1,33 @@
 namespace SpriteKind {
     export const spawn = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0) {
         mySprite.vy = -150
         scene.cameraShake(4, 100)
     }
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`chrJR_exe`,
+    50,
+    true
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`chrL_exe`,
+    50,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`chrJR_exe`,
+    50,
+    true
+    )
 })
 let mySprite: Sprite = null
 tiles.setTilemap(tilemap`level0`)
@@ -28,13 +50,12 @@ mySprite = sprites.create(img`
     5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
     5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
     `, SpriteKind.Player)
-let spawner = sprites.create(assets.image`spawn`, SpriteKind.spawn)
-controller.moveSprite(mySprite, 100, 0)
-mySprite.ay = 300
-scene.cameraFollowSprite(mySprite)
 animation.runImageAnimation(
 mySprite,
-assets.animation`character_exe`,
+assets.animation`chrJR_exe`,
 50,
 true
 )
+controller.moveSprite(mySprite, 100, 0)
+mySprite.ay = 300
+scene.cameraFollowSprite(mySprite)
